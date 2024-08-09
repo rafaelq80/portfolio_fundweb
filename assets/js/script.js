b4b495fd5e1d46fd96b84bab3e8df291c41a3814
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-08-09 00:49:56 -0300

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,9 @@
 ﻿const sobre = document.querySelector("#about");
 
-const formulario = document.querySelector('#formulario');
-
-const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
 async function getApiGithub() {
   try {
     const dadosPerfil = await fetch(
-      "https:api.github.com/users/conteudoGeneration"
+      "https://api.github.com/users/conteudoGeneration"
     );
     const perfil = await dadosPerfil.json();
 
@@ -38,46 +34,5 @@ async function getApiGithub() {
     console.log(error);
   }
 }
-
-formulario.addEventListener('submit', function(event) {
-  
-  event.preventDefault();
-
-  const campoNome = document.querySelector('#name');
-  const txtNome = document.querySelector('#txtNome');
-
-  if (campoNome.value.length < 3) {
-    txtNome.innerHTML = 'O Nome deve ter no minimo 3 caracteres.';
-    campoNome.focus();
-    return;
-  }else{
-    txtNome.innerHTML = '';
-  }
-
-  const campoEmail = document.querySelector('#email');
-  const txtEmail = document.querySelector('#txtEmail');
-
-  if (!campoEmail.value.match(emailRegex)) {
-    txtEmail.innerHTML = 'Digite um E-mail válido.';
-    campoEmail.focus();
-    return;
-  }else{
-    txtEmail.innerHTML = '';
-  }
-
-  const campoSubject = document.querySelector('#subject');
-  const txtSubject = document.querySelector('#txtSubject');
-
-  if (campoSubject.value.length < 5) {
-    txtSubject.innerHTML = 'O Assunto deve ter no minimo 5 caracteres.';
-    campoSubject.focus();
-    return;
-  }else{
-    txtSubject.innerHTML = '';
-  }
-
-  formulario.submit();
-
-});
 
 getApiGithub();
